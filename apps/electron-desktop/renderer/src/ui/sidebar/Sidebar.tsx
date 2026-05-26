e531bee3b1e07d652209e2318397e9dd56653424
Albert-Atomic <k.albert@atomicwallet.io>
2026-05-26 12:49:07 +0300

    change logo icons to red

--- apps/electron-desktop/renderer/src/ui/sidebar/Sidebar.tsx
+++ apps/electron-desktop/renderer/src/ui/sidebar/Sidebar.tsx
@@ -275,7 +275,7 @@ export function Sidebar(props: SidebarProps) {
                   >
                     <path
                       d="M10 2.5L12.5 7.5L18 8L14 12L15 17.5L10 15L5 17.5L6 12L2 8L7.5 7.5L10 2.5Z"
-                      stroke="#c3ff42"
+                      stroke="#ff6464"
                       strokeWidth="1.5"
                       strokeLinecap="round"
                       strokeLinejoin="round"
